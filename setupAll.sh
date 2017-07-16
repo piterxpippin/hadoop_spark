41c7c263e94e2d9f2657fb0853cefee79cd5e0b7
Piotr Pawlak <pieter.pippin@gmail.com>
2017-07-16 21:59:59 +0200

    Fix improper hostnames

--- setupAll.sh
+++ setupAll.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./configureMasterNode.sh ../First_Key_Pair.pem 52.43.162.116 hadoop_namenode
-./configureSlaveNode.sh ../First_Key_Pair.pem 34.211.6.20    hadoop_datanode_1
-./configureSlaveNode.sh ../First_Key_Pair.pem 34.211.102.147 hadoop_datanode_2
-./configureSlaveNode.sh ../First_Key_Pair.pem 35.162.174.117 hadoop_datanode_3
-./configureSlaveNode.sh ../First_Key_Pair.pem 52.26.45.218   hadoop_datanode_4
+./configureMasterNode.sh ../First_Key_Pair.pem 52.43.162.116 namenode
+./configureSlaveNode.sh ../First_Key_Pair.pem 34.211.6.20    datanode1
+./configureSlaveNode.sh ../First_Key_Pair.pem 34.211.102.147 datanode2
+./configureSlaveNode.sh ../First_Key_Pair.pem 35.162.174.117 datanode3
+./configureSlaveNode.sh ../First_Key_Pair.pem 52.26.45.218   datanode4
