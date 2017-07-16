744172b8c01ea9db324af12c0458cd62e1c0f33b
Piotr Pawlak <pieter.pippin@gmail.com>
2017-07-16 14:29:39 +0200

    Another fix

--- externalFunctions.sh
+++ externalFunctions.sh
@@ -48,7 +48,7 @@ function generateSshKeysForHduser() {
 }
 
 function extractHadoopToNode() {
-    if [ ! -e "/tmp/hadoop-2.7.3.tar.gz" ]; then
+    if [ ! -f "/tmp/hadoop-2.7.3.tar.gz" ]; then
         wget http://www-eu.apache.org/dist/hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz /tmp/hadoop-2.7.3.tar.gz
     fi
     tar xf /tmp/hadoop-2.7.3.tar.gz -C /usr/local
