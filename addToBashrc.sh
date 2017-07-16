c5b6489e82ec10e92fcd128d0224ef0589657931
Piotr Pawlak <pieter.pippin@gmail.com>
2017-07-16 19:49:03 +0200

    Final setup?

--- addToBashrc.sh
+++ addToBashrc.sh
@@ -1,5 +1,5 @@
 export HADOOP_HOME=/usr/local/hadoop
-export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0
 
 #Some convenient aliases
 unalias fs &> /dev/null
