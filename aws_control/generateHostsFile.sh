f2b53db25a49d5cae6523aa8bd5f7af42be48038
Piotr Pawlak <pieter.pippin@gmail.com>
2017-08-12 21:59:35 +0200

    Work

--- aws_control/generateHostsFile.sh
+++ aws_control/generateHostsFile.sh
@@ -3,5 +3,5 @@
 aws ec2 describe-instances | python3 -c '
 import sys,json
 for value in json.load(sys.stdin)["Reservations"][1]["Instances"]:
-    print(value["Tags"][0]["Value"], value.get("PrivateIpAddress"), sep=" ", end="\n")
+    print(value.get("PrivateIpAddress"), value["Tags"][0]["Value"], sep=" ", end="\n")
 '
