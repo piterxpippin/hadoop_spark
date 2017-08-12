7d94883d1ab4c8b337f6d369350cdf67ac8084dd
Piotr Pawlak <pieter.pippin@gmail.com>
2017-08-12 19:28:04 +0200

    Work

--- aws_control/listInstances.sh
+++ aws_control/listInstances.sh
@@ -3,5 +3,5 @@
 aws ec2 describe-instances | python3 -c '
 import sys,json
 for value in json.load(sys.stdin)["Reservations"][0]["Instances"]:
-    print(value["Tags"][0]["Value"], value["InstanceId"], value.get("PublicIpAddress"), sep="\t", end="\n")
+    print(value["InstanceId"], value["Tags"][0]["Value"], value.get("PublicIpAddress"), sep="\t", end="\n")
 '
--- setupAll.sh
+++ setupAll.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-./aws_control/listInstances.sh
-
-./configureMasterNode.sh ../First_Key_Pair.pem 52.43.162.116 namenode
-./configureSlaveNode.sh ../First_Key_Pair.pem 34.211.6.20    datanode1
-./configureSlaveNode.sh ../First_Key_Pair.pem 34.211.102.147 datanode2
-./configureSlaveNode.sh ../First_Key_Pair.pem 35.162.174.117 datanode3
-./configureSlaveNode.sh ../First_Key_Pair.pem 52.26.45.218   datanode4
+for instance in $(./aws_control/listInstances.sh); do
+    instanceName=$(echo $instance | awk '{print $2}')
+    instancePublicIp=$(echo $instance | awk '{print $3}')
+    
+    if [ $instanceName == "namenode" ]; then
+        ./configureMasterNode.sh ../First_Key_Pair.pem $instancePublicIp $instanceName
+    else
+        ./configureSlaveNode.sh ../First_Key_Pair.pem $instancePublicIp $instanceName
+    fi
+done
