79c0f555eae032d443018dae83946bf20457d55b
Piotr Pawlak <pieter.pippin@gmail.com>
2017-08-12 20:16:06 +0200

    Work

--- setupAll.sh
+++ setupAll.sh
@@ -10,7 +10,7 @@ for instance in $(./aws_control/listInstances.sh); do
     instanceName=$(echo $instance | awk '{print $2}')
     instancePublicIp=$(echo $instance | awk '{print $3}')
     
-    if [ $instanceName == "namenode" ]; then
+    if [ "$instanceName" == "namenode" ]; then
         ./configureMasterNode.sh ../First_Key_Pair.pem $instancePublicIp $instanceName
     else
         ./configureSlaveNode.sh ../First_Key_Pair.pem $instancePublicIp $instanceName
