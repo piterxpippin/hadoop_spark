c09121d990dd08b55a3efac128ca74c504e50904
Piotr Pawlak <pieter.pippin@gmail.com>
2017-08-12 23:38:58 +0200

    Work

---
+++ runAsHduserOnNamenode.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "You must provide .pem key for logging into machines!"
+    exit 1
+fi
+if [ -z "$2" ]; then
+    echo "You must provide a command!"
+    exit 1
+fi
+keyPath=$1
+command=$2
+
+namenodeAddress=$(aws_control/listInstances.sh | grep namenode | awk '{print $3}')
+ssh -o StrictHostKeyChecking=no -i $keyPath ec2-user@$namenodeAddress "sudo -S -u hduser -i /bin/bash -l -c \"eval '$command'\""
