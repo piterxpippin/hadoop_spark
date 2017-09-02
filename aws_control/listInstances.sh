c5ecdd998dbaf6dda22a6b06907e0ded4cd0eee0
Piotr Pawlak <pieter.pippin@gmail.com>
2017-09-02 15:07:38 +0200

    Work

--- aws_control/listInstances.sh
+++ aws_control/listInstances.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-aws ec2 describe-instances | python3 -c '
+aws ec2 describe-instances --filters 'Name=instance-state-name,Values=pending,running,shutting-down,stopping,stopped' --query 'Reservations[*].Instances[*].[InstanceId,Tags[*].Value,PublicIpAddress,State]' | python3 -c '
 import sys,json
-for value in json.load(sys.stdin)["Reservations"][0]["Instances"]:
-    print(value["InstanceId"], value["Tags"][0]["Value"], value.get("PublicIpAddress"), sep=" ", end="\n")
+for jsonArray in json.load(sys.stdin):
+    for value in jsonArray:
+        print(value[0], value[1][0], value[2], value[3]["Name"], sep="\t", end="\n")
 '
