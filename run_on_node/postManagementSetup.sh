6494e695abd7477f939b11e8b81adf4437c3e04c
Piotr Pawlak <pieter.pippin@gmail.com>
2017-09-02 16:56:09 +0200

    Work

--- run_on_node/postManagementSetup.sh
+++ run_on_node/postManagementSetup.sh
@@ -3,6 +3,7 @@
 REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 . $REPO_DIR/externalFunctions.sh namenode
+
 runAs hduser 'start-dfs.sh'
 runAs hduser 'hdfs dfs -mkdir -p /user/hduser/input'
 runAs hduser 'hdfs dfs -put /home/ec2-user/database/* /user/hduser/input/'
