09c79969792654eb6116556b9754173eda0b71be
Piotr Pawlak <pieter.pippin@gmail.com>
2017-09-02 15:40:42 +0200

    Work

--- configureMasterNode.sh
+++ configureMasterNode.sh
@@ -5,7 +5,7 @@ nodeAddress=$2
 nodeHostname=$3
 
 ssh -o StrictHostKeyChecking=no -i $keyPair ec2-user@$nodeAddress 'sudo yum -y install git wget'
-ssh -o StrictHostKeyChecking=no -i $keyPair ec2-user@$nodeAddress 'chmod -R 740 /home/$HOME/'
+ssh -o StrictHostKeyChecking=no -i $keyPair ec2-user@$nodeAddress 'chmod -R 740 $HOME'
 ssh -o StrictHostKeyChecking=no -i $keyPair ec2-user@$nodeAddress 'if [ -d "$HOME/hadoop_spark" ]; then cd ~/hadoop_spark; git pull -r; else git clone https://github.com/piterxpippin/hadoop_spark.git; fi'
 ssh -o StrictHostKeyChecking=no -i $keyPair ec2-user@$nodeAddress 'mkdir database'
 ssh -o StrictHostKeyChecking=no -i $keyPair ec2-user@$nodeAddress 'cd database; for file in $(curl http://ftp.sunet.se/mirror/archive/ftp.sunet.se/pub/tv+movies/imdb/ | grep --color=never -Po "\>(\K[a-z\.-]+\.gz)"); do wget http://ftp.sunet.se/mirror/archive/ftp.sunet.se/pub/tv+movies/imdb/$file & done; wait'
