c04169bf3d39117934e238dd5d1bddb1b4b39e53
Piotr Pawlak <pieter.pippin@gmail.com>
2017-09-08 23:04:25 +0200

    Work

--- clusterFirstRun.sh
+++ clusterFirstRun.sh
@@ -8,9 +8,10 @@ keyPath=$1
 
 namenodeAddress=$(aws_control/listInstances.sh | grep namenode | awk '{print $3}')
 ssh -o StrictHostKeyChecking=no -i $keyPath ec2-user@$namenodeAddress 'sudo -S -u hduser -i /bin/bash -l -c "start-dfs.sh; start-yarn.sh"'
+sleep 20
 
-ssh -o StrictHostKeyChecking=no -i $keyPath ec2-user@$namenodeAddress 'sudo -S -u hduser -i /bin/bash -l -c "hdfs dfs -mkdir /tmp"'
-ssh -o StrictHostKeyChecking=no -i $keyPath ec2-user@$namenodeAddress 'sudo -S -u hduser -i /bin/bash -l -c "hdfs dfs -mkdir /user/hive/warehouse"'
+ssh -o StrictHostKeyChecking=no -i $keyPath ec2-user@$namenodeAddress 'sudo -S -u hduser -i /bin/bash -l -c "hdfs dfs -mkdir -p /tmp"'
+ssh -o StrictHostKeyChecking=no -i $keyPath ec2-user@$namenodeAddress 'sudo -S -u hduser -i /bin/bash -l -c "hdfs dfs -mkdir -p /user/hive/warehouse"'
 ssh -o StrictHostKeyChecking=no -i $keyPath ec2-user@$namenodeAddress 'sudo -S -u hduser -i /bin/bash -l -c "hdfs dfs -chmod g+w /tmp"'
 ssh -o StrictHostKeyChecking=no -i $keyPath ec2-user@$namenodeAddress 'sudo -S -u hduser -i /bin/bash -l -c "hdfs dfs -chmod g+w /user/hive/warehouse"'
 
@@ -18,8 +19,8 @@ ssh -o StrictHostKeyChecking=no -i $keyPath ec2-user@$namenodeAddress 'sudo -S -
 ssh -o StrictHostKeyChecking=no -i $keyPath ec2-user@$namenodeAddress 'screen -S hiveServerRun -d -m sudo -S -u hduser -i /bin/bash -l -c "hiveserver2"'
 sleep 10
 
-ssh -o StrictHostKeyChecking=no -i $keyPath ec2-user@$namenodeAddress 'screen -S zookeeperRun -d -m sudo -S -u hduser -i /bin/bash -l -c "zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties"'
+ssh -o StrictHostKeyChecking=no -i $keyPath ec2-user@$namenodeAddress 'screen -S zookeeperRun  -d -m sudo -S -u hduser -i /bin/bash -l -c "zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties"'
 sleep 10
-ssh -o StrictHostKeyChecking=no -i $keyPath ec2-user@$namenodeAddress 'screen -S kafkaRun -d -m sudo -S -u hduser -i /bin/bash -l -c "bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties"'
+ssh -o StrictHostKeyChecking=no -i $keyPath ec2-user@$namenodeAddress 'screen -S kafkaRun      -d -m sudo -S -u hduser -i /bin/bash -l -c "kafka-server-start.sh $KAFKA_HOME/config/server.properties"'
 sleep 10
 ssh -o StrictHostKeyChecking=no -i $keyPath ec2-user@$namenodeAddress 'sudo -S -u hduser -i /bin/bash -l -c "kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic testTopic"'
