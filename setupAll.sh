f4387f8fb6572b335042df3d56c8f5d4ee339c96
Piotr Pawlak <pieter.pippin@gmail.com>
2017-09-12 23:22:43 +0200

    Work

--- setupAll.sh
+++ setupAll.sh
@@ -6,7 +6,7 @@ if [ -z "$1" ]; then
 fi
 keyPair=$1
 
-. aws-control/createM4LargeInstances.sh
+. aws-control/createM4xLargeInstances.sh
 while [ $(aws ec2 describe-instance-status | grep "running" | wc -l) != "5" ]; do
     echo "Waiting 5 seconds for all instances' \"running\" state..."
     sleep 5
@@ -26,7 +26,7 @@ for instance in $(aws-control/listInstances.sh); do
     IFS=$OLD_IFS
     instanceName=$(echo $instance | awk '{print $2}')
     instancePublicIp=$(echo $instance | awk '{print $3}')
-    
+
     if [ "$instanceName" != "namenode" ]; then
         ./configureSlaveNode.sh $keyPair $instancePublicIp $instanceName
     else
